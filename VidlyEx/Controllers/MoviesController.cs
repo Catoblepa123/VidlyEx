34a0f9cb4e29e76dcf3119539f5e86b091daefd2
Catoblepa123 <fabiuzliu@gmail.com>
2017-06-20 22:34:54 +0200

    AAA
    AAA

---
+++ VidlyEx/Controllers/MoviesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyEx.Models;
+using VidlyEx.ViewModels;
+
+namespace VidlyEx.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            //return Content("LEPRUNEWAIFU");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+
+            //ViewData["Movie"] = movie;
+            //ViewBag.Movie = movie;
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer1"},
+            new Customer { Name = "Customer2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return (Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy)));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
